55c9f4cb17c7ca9fdef42fe6fb21bdaba694254b
Victoria LeBel <vickileb@hotmail.com>
2022-06-10 16:44:06 -0700

    commit tree 243416b9044442c4caf652c1aee416d386beea14

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+const record = [
+ {year: "2015", result: "W"},
+  {year: "2014", result: "N/A"},
+   {year: "2013", result: "L"},
+]
+// Turned this call back function into arrow function below
+//const callback = (element) => {
+//    return element.result === 'W'
+//}
+
+function superbowlWin (record){
+    const obj = record.find(element => element.result === 'W')
+    if (obj !== undefined) {
+        return obj.year
+    }
+}
+
+console.log(superbowlWin(record))
